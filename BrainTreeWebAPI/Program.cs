a0844f1ac0753eedf56548ae88cba92b115c3f72
nowell <nowell@NOWELL-PC>
2018-11-20 17:21:52 +0800

    commit

---
+++ BrainTreeWebAPI/Program.cs
@@ -0,0 +1,33 @@
+﻿using Braintree;
+
+namespace BrainTreeWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        private string GetToken(string custID)
+        {
+            var gateway = new BraintreeGateway
+            {
+                Environment = Braintree.Environment.SANDBOX,
+                MerchantId = "z72ck693pwd7f8sf",
+                PublicKey = "rqgz9jmsk696563s",
+                PrivateKey = "d58d9d243005e1cab780f4d3c4dcc86a"
+            };
+
+            var clientToken = gateway.ClientToken.Generate(
+            new ClientTokenRequest
+            {
+                CustomerId = custID
+            });
+
+            return clientToken;
+
+        }
+
+    }
+}
